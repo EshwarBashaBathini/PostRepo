0c6e66caef64f241c6762202b6e1b1471ee96f6c
EshwarBashaBathini <160468621+EshwarBashaBathini@users.noreply.github.com>
2024-07-08 10:38:52 +0530

    Update index.jsx

--- src/components/ProtectedRoute/index.jsx
+++ src/components/ProtectedRoute/index.jsx
@@ -1,18 +1,22 @@
-import Cookies from 'js-cookie'
-import { useNavigate, Route } from 'react-router-dom'
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const ProtectedRoute = ({ children }) => {
-  const jwtToken = Cookies.get('jwt_token')
-  console.log(jwtToken)
-  const navigation = useNavigate()
+  const jwtToken = Cookies.get("jwt_token");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!jwtToken) {
+      navigate("/login", { replace: true });
+    }
+  }, [jwtToken, navigate]);
 
   if (!jwtToken) {
-    navigation('/login')
-  } else {
-    return children
+    return null;
   }
 
+  return children;
+};
 
-}
-
-export default ProtectedRoute
+export default ProtectedRoute;
